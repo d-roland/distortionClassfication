aa5bf880411b3492d70a7f551add8ac90108de90
Michael Hsieh <mhsieh33@hotmail.com>
2020-02-10 20:54:39 +0800

    add checkpoints

--- softmaxRegression/softmax.py
+++ softmaxRegression/softmax.py
@@ -1,6 +1,7 @@
 from data.environment import Environment
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense
+from keras.callbacks import ModelCheckpoint
 import os
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # turn off gpu training
@@ -19,16 +20,23 @@ output_dim = nb_classes = 7
 model = Sequential()
 model.add(Dense(output_dim, input_dim=input_dim, activation='softmax'))
 batch_size = 1024
-nb_epoch = 5
+nb_epoch = 20
 
 model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# checkpoint
+filepath="models\softmax.{epoch:02d}."+str(batch_size)+".{val_accuracy:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='max', period=5)
+callbacks_list = [checkpoint]
+
 history = model.fit_generator(env.single_distortion_data_generator(train_list, batch_size=batch_size, flatten=True),
                               steps_per_epoch=len(train_list)/batch_size,
                               epochs=nb_epoch,
                               verbose=1,
                               validation_data=env.single_distortion_data_generator(dev_list, batch_size=batch_size, flatten=True),
-                              validation_steps=len(dev_list)/batch_size)
-model.save("models\softmax."+nb_epoch+"."+batch_size+".h5")
+                              validation_steps=len(dev_list)/batch_size,
+                              callbacks=callbacks_list)
+# model.save("models\softmax."+str(nb_epoch)+"."+str(batch_size)+".h5")
 score = model.evaluate_generator(env.single_distortion_data_generator(test_list, batch_size=batch_size, flatten=True), steps=len(test_list)/batch_size)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
