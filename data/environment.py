c1a493ed1b608b0dcbd33990a599a9674d900177
Michael Hsieh <mhsieh33@hotmail.com>
2020-02-10 14:34:49 +0800

    updates

--- data/environment.py
+++ data/environment.py
@@ -1,19 +1,25 @@
 import os
 import imageio
+import numpy as np
 
 DATA_DIR = "C:/out/"
 
 class Environment:
     def get_single_distortion_data(self):
+        images = None;
         list = os.listdir(DATA_DIR)
         list.sort()
         for f in list:
             filename_parts = f.split(".")
             # skip images with two distortions
-            if(len(filename_parts[1])>1):
+            if(len(filename_parts[1])>1 or filename_parts[1] == "6"):
                 continue
             im = imageio.imread(os.path.join(DATA_DIR, f))
-            print(im.shape)
+            if(images is None):
+                images = np.reshape(im, (1, im.shape[0], im.shape[1], im.shape[2]))
+            else:
+                images = np.append(images, np.reshape(im, (1, im.shape[0], im.shape[1], im.shape[2])), axis=0)
             print(f)
+        print(images.shape)
 env = Environment()
 env.get_single_distortion_data()
