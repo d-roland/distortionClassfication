b383eb91ad41518bd68496699da6ab7bdf5b12c6
Michael Hsieh <mhsieh33@hotmail.com>
2020-02-10 19:46:27 +0800

    initial impl. of softmax

--- data/environment.py
+++ data/environment.py
@@ -5,21 +5,59 @@ import numpy as np
 DATA_DIR = "C:/out/"
 
 class Environment:
-    def get_single_distortion_data(self):
-        images = None;
+    # usage:
+    # env = Environment()
+    # train_list, dev_list, test_list = env.generate_train_dev_test_lists(.95, .025, .025)
+    def generate_train_dev_test_lists(self, ratio_train, ratio_dev, ratio_test, fix_seed=True):
         list = os.listdir(DATA_DIR)
         list.sort()
+        filtered_list = []
         for f in list:
             filename_parts = f.split(".")
-            # skip images with two distortions
-            if(len(filename_parts[1])>1 or filename_parts[1] == "6"):
+            # skip images with two distortions, or "Ripple" distortion #6 since "Ripple" causes image size to change
+            if (len(f) == 0 or len(filename_parts[1]) > 1 or filename_parts[1] == "6"):
                 continue
-            im = imageio.imread(os.path.join(DATA_DIR, f))
-            if(images is None):
-                images = np.reshape(im, (1, im.shape[0], im.shape[1], im.shape[2]))
-            else:
-                images = np.append(images, np.reshape(im, (1, im.shape[0], im.shape[1], im.shape[2])), axis=0)
-            print(f)
-        print(images.shape)
-env = Environment()
-env.get_single_distortion_data()
+            filtered_list.append(f)
+
+        if fix_seed:
+            np.random.seed(1) # let the results be the same
+
+        np.random.shuffle(filtered_list)
+
+        train_pct = float(ratio_train) / float(ratio_train+ratio_dev+ratio_test)
+        dev_pct = float(ratio_dev) / float(ratio_train+ratio_dev+ratio_test)
+
+        train_index = int(train_pct*len(filtered_list))
+        dev_index = int((dev_pct+train_pct)*len(filtered_list))
+
+        return filtered_list[:train_index], filtered_list[train_index:dev_index], filtered_list[dev_index:]
+
+    # usage:
+    # for (images, labels) in env.single_distortion_data_generator(train_list):
+    def single_distortion_data_generator(self, list, batch_size=1000, flatten=False):
+        images = []
+        labels = []
+        while True:
+            imageNumber = 0
+            while imageNumber < len(list):
+                f = list[imageNumber]
+                filename_parts = f.split(".")
+                label = filename_parts[1]
+                if label == "7":
+                    label = "6"
+                im = imageio.imread(os.path.join(DATA_DIR, f))
+                if(flatten):
+                    im=im.flatten()
+                images.append(im)
+                # images.append(np.reshape(im, (1, im.shape[0], im.shape[1], im.shape[2])))
+                one_hot_label = np.zeros(7)
+                one_hot_label[int(label)] = 1
+                labels.append(one_hot_label)
+                if (len(images) >= batch_size):
+                    # print(np.array(images).shape)
+                    # print(np.array(labels).shape)
+                    yield (np.array(images), np.array(labels))
+                    images = []
+                    labels = []
+                imageNumber += 1
+                # print(imageNumber)
---
+++ softmaxRegression/softmax.py
@@ -0,0 +1,30 @@
+from data.environment import Environment
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+env = Environment()
+train_list, dev_list, test_list = env.generate_train_dev_test_lists(.95, .025, .025)
+
+# for (images, labels) in env.single_distortion_data_generator(train_list, flatten=True):
+#     print(images.shape)
+print(len(train_list))
+print(len(dev_list))
+print(len(test_list))
+
+input_dim = 224*224*3
+output_dim = nb_classes = 7
+model = Sequential()
+model.add(Dense(output_dim, input_dim=input_dim, activation='softmax'))
+batch_size = 1024
+nb_epoch = 5
+
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit_generator(env.single_distortion_data_generator(train_list, batch_size=batch_size, flatten=True),
+                              steps_per_epoch=len(train_list)/batch_size,
+                              epochs=nb_epoch,
+                              verbose=1,
+                              validation_data=env.single_distortion_data_generator(dev_list, batch_size=batch_size, flatten=True),
+                              validation_steps=len(dev_list)/batch_size)
+score = model.evaluate_generator(env.single_distortion_data_generator(test_list, batch_size=batch_size, flatten=True), steps=len(test_list)/batch_size)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
