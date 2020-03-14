62d7dcc80fd2ec436f720430e523f192d28cc077
d1moooo <58936791+d1moooo@users.noreply.github.com>
2020-03-14 16:34:46 +0100

    Update sgdnet_transfer.py

--- sgdnet_transfer/sgdnet_transfer.py
+++ sgdnet_transfer/sgdnet_transfer.py
@@ -1,4 +1,4 @@
-# These imports can be skipped if not running from a Jupiter notebook
+# These first 6 imports can be skipped if not running from a Jupiter notebook
 # Instead, simply use the following:
 # from data.environment import Environment
 from __future__ import division
