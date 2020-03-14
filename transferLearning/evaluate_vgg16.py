b832718ff559c0c62a547606ba97decac587df3a
d1moooo <58936791+d1moooo@users.noreply.github.com>
2020-03-14 19:12:35 +0100

    Update and rename evaluate_vgg16-2.py to evaluate_vgg16.py

--- transferLearning/evaluate_vgg16.py
+++ transferLearning/evaluate_vgg16.py
@@ -3,7 +3,7 @@ from keras.models import load_model
 from argparse import ArgumentParser
 from math import ceil
 
-""" Evaluator of VGG model 2 (based on 3 labels and dataset v2)
+""" Evaluator of VGG transfered model (based on 8 or 3 classes)
     Outputs loss and accuracy on specified test set
 """
 
@@ -11,7 +11,7 @@ from math import ceil
 # Instead, simply use the following:
 # from data.environment import Environment
 import importlib.util
-spec = importlib.util.spec_from_file_location("Environment", "/home/jupyter/Env/keras_ve/transfer-learning/data/environment-dir2.py")
+spec = importlib.util.spec_from_file_location("Environment", "/home/jupyter/Env/keras_ve/transfer-learning/data/environment.py")
 foo = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(foo)
 env = foo.Environment()
@@ -33,9 +33,10 @@ train_list, dev_list, test_list = env.generate_train_dev_test_lists(args.data_di
 
 # Load relevant model for evaluation
 
-#custom_model = load_model('vgg16-d3-l1-RMSProp-06-256-0.8408-0.36.i.h5') 
-# custom_model = load_model('vgg16scratch-d3-l1-RMSProp-04-128-0.8410-0.34.k.h5') 
-custom_model = load_model('vgg16scratch-d3-l1-RMSProp-04-128-0.8410-0.34.k.h5') 
+# custom_model = load_model('vgg16-d0-adam-05-128-0.9892-0.00.d.h5') 
+# custom_model = load_model('vgg16-d0-RMSProp-05-256-0.99-0.25.c.h5') 
+# custom_model = load_model('vgg16-d1-l1-RMSProp-06-256-0.8408-0.36.i.h5') 
+custom_model = load_model('vgg16scratch-d1-l1-RMSProp-04-128-0.8410-0.34.k.h5') 
 
 # Set batch size and steps number
 batch_size = 128
